b061ec76883337460064e649b8e1a229d3d12283
grimjaww <46424306+grimjaww@users.noreply.github.com>
2019-05-03 17:41:37 +0200

    Update assgn_1.2.py

--- assgn_1.2.py
+++ assgn_1.2.py
@@ -9,11 +9,11 @@ j = 0
 i = 0
 
 for i in range(len(x)):
-    #print('I:', i)
+    #print('int I:', i)
     #print('X: ',x[i])
     #print('y: ',y[j])
     #print(temp)
-    if i < m and y[j] == x[i]:
+    if i < m + 1 and y[j] == x[i]:
         result += x[i]
         j = j + 1
         i = i + 1
@@ -23,25 +23,31 @@ for i in range(len(x)):
         result += x[i]
         j = j + 1
         i = i + 1
-        temp = x[i]
+        if i != m:
+            temp = x[i]
     elif y[j] == '*':
         if i == 0 or temp == 'Null':
             j = j + 1
             break
-        if  i != m:
+        if  i != m + 1:
             if temp == x[i]:
-                #print(temp)
-                #print(x[i])
                 result += x[i]
                 i = i + 1
                 #temp = x[i]
-                if i == m-1:
-                    break
+                #if i == m:
+                    #break
         #print('I: ',i)
-            if i < m and (temp != x[i - 1]):
+            #if i < m and (temp != x[i - 1]):
+            else:
                 j = j + 1
+                i = i - 1
     else:
         break
+    print("Temp: ",temp)
+    #print("X: ",x[i])
+    #print("I: ", i)
+    #print("J: ", j)                
+    print("Result: ", result)
 #loop = loop + 1
 
 print('Result: ',result)
