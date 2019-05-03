4e7064209f6b1963331e4348c2d9817d30d2fb7a
grimjaww <46424306+grimjaww@users.noreply.github.com>
2019-05-03 15:27:58 +0200

    Update assgn_1.2.py

--- assgn_1.2.py
+++ assgn_1.2.py
@@ -4,32 +4,48 @@ y = input("Enter a pattern: ")
 m = len(x)
 
 result = ''
-#loop, count = 0, 0
+temp = 'Null'
 j = 0
 i = 0
 
 for i in range(len(x)):
-    #print(i)
-    if x[i] == y[j]:
+    #print('I:', i)
+    #print('X: ',x[i])
+    #print('y: ',y[j])
+    #print(temp)
+    if i < m and y[j] == x[i]:
         result += x[i]
         j = j + 1
+        i = i + 1
+        #if i < m:
+        temp = x[i] 
     elif y[j] == '.':
         result += x[i]
         j = j + 1
+        i = i + 1
+        temp = x[i]
     elif y[j] == '*':
-        if i == 0:
+        if i == 0 or temp == 'Null':
+            j = j + 1
             break
         if  i != m:
-            if x[i] == x[i + 1]:
+            if temp == x[i]:
+                #print(temp)
+                #print(x[i])
                 result += x[i]
                 i = i + 1
+                #temp = x[i]
                 if i == m-1:
                     break
+        #print('I: ',i)
+            if i < m and (temp != x[i - 1]):
+                j = j + 1
     else:
         break
 #loop = loop + 1
 
-if result in x:
+print('Result: ',result)
+if x == result:
     print('True')
 else:
     print('False')
