f7588fa78cf4c04457825f1b962cdecfe4d1f409
grimjaww <46424306+grimjaww@users.noreply.github.com>
2019-05-03 13:00:50 +0200

    Update assgn_1.2.py

--- assgn_1.2.py
+++ assgn_1.2.py
@@ -1,10 +1,15 @@
 x = input("Enter a string: ")
 y = input("Enter a pattern: ")
 
+m = len(x)
+
 result = ''
+loop, count = 0, 0
 j = 0
+i = 0
 
 for i in range(len(x)):
+    #print(i)
     if x[i] == y[j]:
         result += x[i]
         j = j + 1
@@ -12,9 +17,17 @@ for i in range(len(x)):
         result += x[i]
         j = j + 1
     elif y[j] == '*':
-        if x[i] == x[i + 1] and i < len(x):
-            result += x[i]
-            i = i + 1
+        if i == 0:
+            break
+        if  i != m:
+            if x[i] == x[i + 1]:
+                result += x[i]
+                i = i + 1
+                if i == m-1:
+                    break
+    else:
+        break
+loop = loop + 1
 
 if result in x:
     print('True')
